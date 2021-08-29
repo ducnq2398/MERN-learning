8841f5d210c4d9f99192f605d3e94a7f4e0c1463
ducnq2398 <ducnq2398@gmail.com>
2021-08-30 06:02:56 +0700

    add frontend

---
+++ client/src/app/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from '../containers/Login/Login';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/containers/Login/Login.js
@@ -0,0 +1,10 @@
+import '../../css/login.css';
+function Login(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login;
